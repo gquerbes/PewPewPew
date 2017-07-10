d05817f242fa357e4cac4953f8b97f5043a60317
Gabriel <gquerbes@gmail.com>
2017-07-09 20:00:13 -0400

    Added randomizations in color and location

--- PewPewPew/MainScene.swift
+++ PewPewPew/MainScene.swift
@@ -18,9 +18,14 @@ class MainScene : SKScene, SKPhysicsContactDelegate{
     private var paddleXPosition : CGFloat?
     private var paddleYPosition : CGFloat? // does not change
     private var lastUpdate : TimeInterval = 0
+    private var score : Int32 = 0
+    private var scoreLabel : SKLabelNode?
     
     
     override func sceneDidLoad() {
+        //get reference to score label from MainScene.sks
+         self.scoreLabel = self.childNode(withName: "//ScoreLabel") as? SKLabelNode
+        
         // create paddle
         if(paddle == nil){
             let w = (self.size.width + self.size.height) * 0.05
@@ -48,19 +53,48 @@ class MainScene : SKScene, SKPhysicsContactDelegate{
     
     
 
-    private var isRed : Bool = false
     func dropBall(){
         if let newball = self.ball?.copy() as! Ball?{
-            newball.setColor(color: isRed ? UIColor.red : UIColor.blue)
+            newball.setColor(color: getRandomColor())
             newball.position = CGPoint(x: getRandomPosition(), y: newball.position.y)
             self.addChild(newball)
-            isRed = !isRed
         }
     }
     
     func getRandomPosition() -> CGFloat{
         let x =  GKRandomDistribution.init(lowestValue: Int((self.size.width)/2) * -1, highestValue: Int((self.size.width)/2) - 10).nextInt()
         return CGFloat(x)
+    }
+    
+    
+    var colorRed = "";
+    var colorBlue = "";
+    var colorOrange = "";
+    var colorBlack = "";
+    var colorCyan = "";
+    func getRandomColor() -> UIColor{
+        print("\nred \(colorRed)\nblue\(colorBlue)\norange \(colorOrange)\ncyan \(colorCyan)\nblack \(colorBlack)")
+        let x = GKShuffledDistribution.init(lowestValue: 0, highestValue: 50)
+        
+        print(x.nextInt())
+
+        switch x.nextInt() {
+        case 0..<10:
+            colorRed += "*"
+            return UIColor.red
+        case 10..<20:
+            colorBlue += "*"
+            return UIColor.blue
+        case 20..<30:
+            colorOrange += "*"
+            return UIColor.orange
+        case 30..<40:
+            colorCyan += "*"
+            return UIColor.cyan
+        default:
+            colorBlack += "*"
+            return UIColor.black
+        }
     }
     
     
@@ -121,11 +155,18 @@ class MainScene : SKScene, SKPhysicsContactDelegate{
             let ball = contact.bodyB.node as! Ball
             
             if(ball.color == UIColor.red){
-                paddle?.shrink(byAmount: 5)
+                paddle?.shrink(byAmount: 15)
+                score -= 10
+            }
+            else if(ball.color == UIColor.blue){
+                paddle?.grow(byAmount: 5)
+                score += 10
             }
             else{
-                paddle?.grow(byAmount: 5)
+                score += 5
             }
+            //update score
+            updateScoreLabel()
 
             //remove ball
             ball.removeFromParent()
@@ -138,6 +179,11 @@ class MainScene : SKScene, SKPhysicsContactDelegate{
             self.addChild(shockwave)
             shockwave.run(shockWaveAction)
         }
+    }
+    
+    func updateScoreLabel() {
+        scoreLabel?.text =  String(score)
+        scoreLabel?.run(SKAction.sequence([SKAction.fadeOut(withDuration: 0.25), SKAction.fadeIn(withDuration: 0.5)]))
     }
     
 }
